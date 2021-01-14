6a8ba09d17764613ac78e8950966d80377146663
charismaua <alina.ytproducer@gmail.com>
2021-01-15 00:31:15 +0100

    Improve code readability

--- dark-mode/src/routes/App/components/App.js
+++ dark-mode/src/routes/App/components/App.js
@@ -3,6 +3,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon } from '@fortawesome/free-solid-svg-icons';
 import '../styles/_app.scss';
 
+const Field = ({ placeholder }) => (
+  <div className="field">
+    <div className="control">
+      <input className="input" type="text" placeholder={placeholder} />
+    </div>
+  </div>
+);
+
+const Column = ({ children }) => (
+  <div className="column">
+    <p>{children}</p>
+  </div>
+);
+
 function App() {
   return (
     <div className="app">
@@ -15,29 +29,28 @@ function App() {
         <button className="app__dark-mode-btn icon level-right">
           <FontAwesomeIcon icon={faMoon} />
         </button>
-
       </div>
 
       <div className="columns">
-        <div className="column">
-          <p>Lollipop powder powder. Cotton candy caramels chupa chups halvah muffin caramels apple pie topping cake. Topping chocolate bar pastry chocolate cake. Cupcake tart jujubes dragée jelly-o icing sugar plum. Chocolate bar lollipop candy canes. Biscuit croissant apple pie pudding caramels wafer tart tootsie roll macaroon. Croissant tiramisu chocolate bar carrot cake lemon drops halvah.</p>
-        </div>
-        <div className="column">
-          <p>Marshmallow tiramisu liquorice bear claw chocolate bar bear claw tart. Muffin chupa chups pie. Brownie apple pie topping lemon drops marzipan toffee. Pudding macaroon icing ice cream bonbon cake tart. Pudding sugar plum chocolate cake cake biscuit pastry pastry chocolate bar tart. Lemon drops dessert gummies icing.</p>
-        </div>
+        <Column>
+          Lollipop powder powder. Cotton candy caramels chupa chups halvah
+          muffin caramels apple pie topping cake. Topping chocolate bar pastry
+          chocolate cake. Cupcake tart jujubes dragée jelly-o icing sugar plum.
+          Chocolate bar lollipop candy canes. Biscuit croissant apple pie
+          pudding caramels wafer tart tootsie roll macaroon. Croissant tiramisu
+          chocolate bar carrot cake lemon drops halvah.
+        </Column>
+        <Column>
+          Marshmallow tiramisu liquorice bear claw chocolate bar bear claw tart.
+          Muffin chupa chups pie. Brownie apple pie topping lemon drops marzipan
+          toffee. Pudding macaroon icing ice cream bonbon cake tart. Pudding
+          sugar plum chocolate cake cake biscuit pastry pastry chocolate bar
+          tart. Lemon drops dessert gummies icing.
+        </Column>
       </div>
 
-      <div className="field">
-        <div className="control">
-          <input className="input" type="text" placeholder="Name" />
-        </div>
-      </div>
-
-      <div className="field">
-        <div className="control">
-          <input className="input" type="text" placeholder="Email" />
-        </div>
-      </div>
+      <Field placeholder="Name" />
+      <Field placeholder="Email" />
 
       <section className="section">
         <div className="buttons level-right">
